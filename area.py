ad863055ab228b3b42393c59d6330d4735f6776c
Divyanshii <divyanshiiarya23@gmail.com>
2023-01-30 19:36:32 +0530

    first commit

---
+++ area.py
@@ -0,0 +1,10 @@
+d = float(input("diameter:"))
+r = d/2
+pi = 22/7
+area = pi*r*r
+cir = 2*pi*r
+print (f'diameter:{d}')
+print (f'radius:{r}')
+print (f'pi:{pi}')
+print (f'area:{area}')
+print (f'circumference:{cir}')
---
+++ si.py
@@ -0,0 +1,9 @@
+p = int(input("principal:"))
+r = int(input("rate:"))
+t = int(input ("time:"))
+si = (p*r*t)/100
+print ("simple interest calc")
+print (f'{p=}')
+print (f'{r=}')
+print (f'{t=}')
+print (f'simple interest:{si}')
